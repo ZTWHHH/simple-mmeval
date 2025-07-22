10442b41aa46ebd76a086ce30b5aa7d3c0fe3107
ZTWHHH <ztw371@gmail.com>
2025-07-22 22:37:10 +0000

    update evalkit example script

--- scripts/test_bed/qwen2d5-evalkit.sh
+++ scripts/test_bed/qwen2d5-evalkit.sh
@@ -1,10 +1,14 @@
 export PYTHONPATH=./:$PYTHONPATH
 
-# test MMBench_dev_en, MMMB_en, POPE
-# need login huggingface
+# Before running this script, you need to create .env file in the root directory.
+# And set DATASET_DIR in .env as tsv file local directory.
+# Need login huggingface.
+# dataset example: evalkit@MMBench_dev_en, evalkit@MMMB_en, evalkit@POPE.
+# dataset example: evalkit@https://huggingface.co/datasets/mm-eval/VLMEvalKit/resolve/main/3DSRBench.tsv
+
 python mmeval/run.py \
-    --dataset POPE \
-    --out_dir work_dirs/qwen2d5_POPE_test \
+    --dataset evalkit@MMBench_dev_en \
+    --out_dir work_dirs/qwen2d5_MMBench_dev_en_test \
     --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
     --gpu_per_parallel 1 \
     --parallel_per_task 1 
