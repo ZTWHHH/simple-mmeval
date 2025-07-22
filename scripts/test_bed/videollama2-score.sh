9ee99bc07c990e296b3c16e835f4bc69a2cc60be
snorfyang <snorfyang@gmail.com>
2025-07-22 04:32:00 +0000

    script

---
+++ scripts/test_bed/videollama2-score.sh
@@ -0,0 +1,9 @@
+export PYTHONPATH=./:$PYTHONPATH
+
+python mmeval/infer/videollama2.py \
+    --infile test_bed/video-qca.json \
+    --dataset local@json \
+    --out_dir test_bed/test_videollama2 \
+    --img_dir test_bed \
+    --model_name_or_path DAMO-NLP-SG/VideoLLaMA2-7B \
+    --score_target True
