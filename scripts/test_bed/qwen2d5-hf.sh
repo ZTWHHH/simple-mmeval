01099fdde798166f81099f05c6a7e117d9309110
IcyWang <irisicy@outlook.com>
2025-08-10 16:51:34 +0000

    add result

--- scripts/test_bed/qwen2d5-hf.sh
+++ scripts/test_bed/qwen2d5-hf.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=./:$PYTHONPATH
 
 python mmeval/run.py \
     --dataset mmeval_hf@mm-eval/MMBench-en \
-    --split dev \
+    --split test \
     --out_dir work_dirs/qwen2d5-mmeval_hf_MMBench_en_train \
     --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
     --gpu_per_parallel 1 \
