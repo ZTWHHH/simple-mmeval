3e6f10e241c464cba108cbff3e34a3435d55558a
snorfyang <snorfyang@gmail.com>
2025-10-13 20:52:03 +0000

    update modality test

--- scripts/test_bed/smolvlm.sh
+++ scripts/test_bed/smolvlm.sh
@@ -1,9 +1,10 @@
 export PYTHONPATH=./:$PYTHONPATH
 
 python mmeval/run.py \
-    --dataset mmeval_hf@mm-eval/MMMU \
-    --split validation \
-    --out_dir work_dirs/smolvlm-mmeval_hf_MMMU_val \
+    --infile test_bed/modality_test/task/single_image_start.json \
+    --dataset local@json \
+    --out_dir work_dirs/smolvlm-single-image-start \
+    --img_dir test_bed/modality_test/media/448 \
     --model_name_or_path HuggingFaceTB/SmolVLM-Instruct \
     --gpu_per_parallel 1 \
     --parallel_per_task 4 \
