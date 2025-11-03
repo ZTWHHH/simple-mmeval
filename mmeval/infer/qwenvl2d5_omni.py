e0879149cd88e566fe49be92f1c81aad900e4d70
ZTWHHH <ztw371@gmail.com>
2025-11-03 00:32:08 +0000

    update qwen infer script, update registry

---
+++ mmeval/infer/qwenvl2d5_omni.py
@@ -0,0 +1,128 @@
+import re
+import copy
+
+import torch
+import numpy as np
+from transformers import Qwen2_5OmniForConditionalGeneration, Qwen2_5OmniProcessor
+from qwen_omni_utils import process_mm_info
+
+from mmeval.infer.task import Task
+from mmeval.utils import constants
+from mmeval.utils.argparser import parse_args, parse_model_kwargs, parse_gen_kwargs
+from mmeval.utils.scorer import IncrementalLMScorer, target_tokens
+
+# set use audio in video
+USE_AUDIO_IN_VIDEO = False
+
+class TaskRunner(Task):
+    def __init__(self, args):
+        self.args = args
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.dtype = getattr(args, "dtype") or "auto"
+        self.default_model_kwargs = {"attn_implementation":"flash_attention_2", "device_map": "auto"}
+        self.default_gen_kwargs = {"max_new_tokens": 128}
+        self.model_kwargs = parse_model_kwargs(args, self.default_model_kwargs)
+        self.gen_kwargs = parse_gen_kwargs(args, self.default_gen_kwargs)
+
+        super().__init__(args)
+        
+    def load_model(self, args):
+        self.model = Qwen2_5OmniForConditionalGeneration.from_pretrained(args.model_name_or_path, torch_dtype=self.dtype, **self.model_kwargs)
+        self.processor = Qwen2_5OmniProcessor.from_pretrained(args.model_name_or_path)
+
+    def parse_input(self, sample:dict):
+        question = sample["prompt"]
+        # placeholder <>, can be image, video, etc.
+        q_chunks = re.split(r'(<(?:image|video)>)', question)
+        media_list = copy.deepcopy(sample['media'])
+
+        conversation = [
+            {
+                "role": "system",
+                "content": [
+                    {"type": "text", "text": "You are Qwen, a virtual human developed by the Qwen Team, Alibaba Group, capable of perceiving auditory and visual inputs, as well as generating text and speech."}
+                ],
+            },
+            {
+                "role": "user",
+                "content": []
+            }
+        ]
+
+        for chunk in q_chunks:
+            if len(chunk.strip()) == 0:
+                continue
+            if chunk == constants.image:
+                media = media_list.pop(0)
+                conversation[1]["content"].append(
+                    {
+                        "type": "image",
+                        "image": media
+                    }
+                )       
+            elif chunk == constants.video:
+                media = media_list.pop(0)
+                conversation[1]["content"].append(
+                    {
+                        "type": "video",
+                        "video": media,
+                        "max_pixels": 360 * 420,
+                        "fps": 1.0,
+                    }
+                )
+            else:
+                conversation[1]["content"].append(
+                    {
+                        "type": "text",
+                        "text": chunk
+                    }
+                )
+
+        return conversation
+
+    def _generate_response(self, inputs):
+        # Inference: Generation of the output text and audio
+        text_ids, audio = self.model.generate(**inputs, use_audio_in_video=USE_AUDIO_IN_VIDEO, **self.gen_kwargs)
+
+        text = self.processor.batch_decode(text_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+
+        return text
+
+    # def _score_choices(self, text, image_inputs, video_inputs, sample):
+    #     contents = sample.get("choices")
+    #     full = [text + content for content in contents]
+
+    #     full_encoded = [self.processor(text=i, images=image_inputs, videos=video_inputs, return_tensors="pt").to(self.device) for i in full]
+    #     prompt_encoded = self.processor(text=text, images=image_inputs, videos=video_inputs, return_tensors="pt").to(self.device)
+    #     target_toks = target_tokens(self.tokenizer, contents)
+
+    #     scorer = IncrementalLMScorer(self.model, self.device, tokenizer=self.tokenizer)
+    #     scores = scorer.conditional_score(target_toks, full_encoded, prompt_encoded)
+        
+    #     return {
+    #         "score": scores,
+    #         "response": contents[np.argmax(scores)]
+    #     }
+
+    def run_sample(self, sample: dict):
+        ori_sample = copy.deepcopy(sample)
+        conversation = self.parse_input(ori_sample)
+
+        # Preparation for inference
+        text = self.processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+        audios, images, videos = process_mm_info(conversation, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+        inputs = self.processor(text=text, audio=audios, images=images, videos=videos, return_tensors="pt", padding=True, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+        inputs = inputs.to(self.model.device).to(self.model.dtype)
+
+        if not self.args.score_target:
+            ori_sample["response"] = self._generate_response(inputs)
+        else:
+            pass
+
+        return ori_sample
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    model_evaluator = TaskRunner(args)
+    model_evaluator.inference_dataset()
